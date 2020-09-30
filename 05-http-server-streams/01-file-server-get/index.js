5f6718137bada150efc4a4594ac030599c7e22cb
javascriptru <67475495+jsru-1@users.noreply.github.com>
2020-09-30 23:55:05 +0300

    Added task Файловый сервер - отдача файла

---
+++ 05-http-server-streams/01-file-server-get/index.js
@@ -0,0 +1,5 @@
+const server = require('./server');
+
+server.listen(3000, () => {
+  console.log('Server is listening on http://localhost:3000');
+});
---
+++ 05-http-server-streams/01-file-server-get/server.js
@@ -0,0 +1,23 @@
+const url = require('url');
+const http = require('http');
+const path = require('path');
+
+const server = new http.Server();
+
+server.on('request', (req, res) => {
+  const pathname = url.parse(req.url).pathname.slice(1);
+
+  const filepath = path.join(__dirname, 'files', pathname);
+
+  switch (req.method) {
+    case 'GET':
+
+      break;
+
+    default:
+      res.statusCode = 501;
+      res.end('Not implemented');
+  }
+});
+
+module.exports = server;

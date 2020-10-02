e0253560201d7de4ae691ef52a3047f825bc2817
Sviataslau Ushev <ushev.s90@gmail.com>
2020-10-02 23:57:06 +0300

    add solution for 5-module 1-task

--- 05-http-server-streams/01-file-server-get/server.js
+++ 05-http-server-streams/01-file-server-get/server.js
@@ -1,22 +1,42 @@
 const url = require('url');
 const http = require('http');
 const path = require('path');
+const fs = require('fs');
 
 const server = new http.Server();
 
 server.on('request', (req, res) => {
-  const pathname = url.parse(req.url).pathname.slice(1);
+  try {
+    let pathname = url.parse(req.url).pathname.slice(1);
+    if (pathname.indexOf('/') >= 0) {
+      res.statusCode = 400;
+      res.end('Your have to send only a filename!');
+      return;
+    }
+    const filepath = path.join(__dirname, 'files', pathname);
 
-  const filepath = path.join(__dirname, 'files', pathname);
+    switch (req.method) {
+      case 'GET':
+        const readStream = fs.createReadStream(filepath).on('error', (err) => {
+          if (err.code === 'ENOENT') {
+            res.statusCode = 404;
+            res.end('No such file or directory');
+          } else {
+            res.statusCode = 500;
+            res.end('Internal Server Error');
+          }
+        });
+        readStream.pipe(res);
+        break;
 
-  switch (req.method) {
-    case 'GET':
-
-      break;
-
-    default:
-      res.statusCode = 501;
-      res.end('Not implemented');
+      default:
+        res.statusCode = 501;
+        res.end('Not implemented');
+    }
+  } catch (err) {
+    console.log(err);
+    res.statusCode = 500;
+    res.end('Internal Server Error');
   }
 });
 

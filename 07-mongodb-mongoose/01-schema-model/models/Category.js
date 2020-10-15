b9de008a868769842e616e7157c9cca184984676
Sviataslau Ushev <ushev.s90@gmail.com>
2020-10-15 23:04:31 +0300

    add solution for 7-module 1-task

--- 07-mongodb-mongoose/01-schema-model/models/Category.js
+++ 07-mongodb-mongoose/01-schema-model/models/Category.js
@@ -2,9 +2,17 @@ const mongoose = require('mongoose');
 const connection = require('../libs/connection');
 
 const subCategorySchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true
+  }
 });
 
 const categorySchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+  },
   subcategories: [subCategorySchema],
 });
 
--- 07-mongodb-mongoose/01-schema-model/models/Product.js
+++ 07-mongodb-mongoose/01-schema-model/models/Product.js
@@ -1,7 +1,30 @@
 const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
 const connection = require('../libs/connection');
 
-const productSchema = new mongoose.Schema({
+const productSchema = new Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  price: {
+    type: Number,
+    required: true,
+  },
+  category: {
+    type: Schema.Types.ObjectId,
+    ref: 'Category',
+    required: true,
+  },
+  subcategory: {
+    type: Schema.Types.ObjectId,
+    required: true,
+  },
+  images: [String]
 });
 
 module.exports = connection.model('Product', productSchema);

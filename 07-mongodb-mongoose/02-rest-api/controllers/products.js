f832d6c8e29df3478000d12b916a547a20ada6bf
Sviataslau Ushev <ushev.s90@gmail.com>
2020-10-16 00:10:13 +0300

    added solution for 7-module 2-task

--- 07-mongodb-mongoose/02-rest-api/controllers/products.js
+++ 07-mongodb-mongoose/02-rest-api/controllers/products.js
@@ -1,12 +1,40 @@
-module.exports.productsBySubcategory = async function productsBySubcategory(ctx, next) {
-  ctx.body = {};
+var mongoose = require('mongoose');
+const Product = require('../models/Product');
+
+module.exports.productsBySubcategory = async function productsBySubcategory(
+  ctx,
+  next
+) {
+  const { subcategory } = ctx.request.query;
+  if (!subcategory) {
+    ctx.response.status = 400;
+    ctx.body = 'Please, specify subcategory param';
+  } else {
+    const products = await Product.find({ subcategory });
+    ctx.response.status = 200;
+    ctx.body = { products };
+  }
 };
 
 module.exports.productList = async function productList(ctx, next) {
-  ctx.body = {};
+  const products = await Product.find({});
+  ctx.response.status = 200;
+  ctx.body = { products };
 };
 
 module.exports.productById = async function productById(ctx, next) {
-  ctx.body = {};
+  const { id } = ctx.params;
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    ctx.response.status = 400;
+    ctx.body = 'Invalid product ID';
+  } else {
+    const product = await Product.findById(id);
+    if (!product) {
+      ctx.response.status = 404;
+      ctx.body = 'Product not found';
+    } else {
+      ctx.response.status = 200;
+      ctx.body = { product };
+    }
+  }
 };
-
